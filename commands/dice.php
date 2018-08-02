8b533cbb93390efa0eca1f5a468f1ce94b220649
Jonas Regner <jonas.regner@dotsunited.de>
2018-08-02 15:37:26 +0200

    test command

--- commands/dice.php
+++ commands/dice.php
@@ -6,5 +6,7 @@ use BotMan\BotMan\BotMan;
 $botman->hears('{name} roll the dice', function (BotMan $bot, $name) {
     if ($name === '@UC1QKMCPR') {
         $bot->reply(random_int(1, 6));
+    } else {
+        $bot->reply(str_rot13($name));
     }
 });
