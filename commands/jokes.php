14dbe24aff13346668f2726e0960b37983684da7
Jonas Regner <jonas.regner@dotsunited.de>
2018-08-02 16:19:04 +0200

    add new command to jokes

--- commands/jokes.php
+++ commands/jokes.php
@@ -10,6 +10,13 @@ $botman->hears('tell me a joke', function (BotMan $bot) {
     $bot->reply($object->value->joke);
 });
 
+$botman->hears('tell me a fred', function (BotMan $bot) {
+    $response = file_get_contents('http://api.icndb.com/jokes/random');
+    $object = json_decode($response);
+
+    $bot->reply(str_replace('Chuck Norris', 'Fred', $object->value->joke));
+});
+
 $botman->hears('.*\b(Essen|essen)\b.*', function (BotMan $bot, $name) {
     $bot->reply(':chompy: :pizza:');
 });
