b56e887dca2461f97cff36a689f098beb37ce8b5
Jonas Regner <jonas.regner@dotsunited.de>
2018-08-02 11:31:52 +0200

    fixes commands

--- commands/helpers.php
+++ commands/helpers.php
@@ -5,6 +5,6 @@ use BotMan\BotMan\BotMan;
 /** @var $botman BotMan */
 $botman->hears('time', function (BotMan $bot) {
     $date = new DateTime('now', new DateTimeZone('Europe/Berlin'));
-    $bot->reply($date->format('l, d. F Y (W) H:i:s'));
+    $bot->reply($date->format('l, d. F Y (W. \C\W) H:i:s'));
 });
 
