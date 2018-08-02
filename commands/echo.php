1540a0baf664786109eacb2307012d25aae6986d
Jonas Regner <jonas.regner@dotsunited.de>
2018-08-02 10:15:19 +0200

    rm echo logic

--- commands/echo.php
+++ commands/echo.php
@@ -6,9 +6,3 @@ use BotMan\BotMan\BotMan;
 $botman->hears('^echo (.*)', function (BotMan $bot, $echo) {
    $bot->reply($echo);
 });
-
-$botman->on('message', function($payload, BotMan $bot) {
-    if (1 === preg_match('/^echo (.*)/', $payload['event']['text'], $matches)) {
-        $bot->say($matches[0], $payload['event']['channel'], \BotMan\Drivers\Slack\SlackDriver::class);
-    }
-});
