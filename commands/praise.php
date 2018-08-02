d71dd459d3632193c28218f1bf04656989e7c951
Jonas Regner <jonas.regner@dotsunited.de>
2018-08-02 10:28:42 +0200

    try fix new commands

--- commands/praise.php
+++ commands/praise.php
@@ -3,22 +3,22 @@
 use BotMan\BotMan\BotMan;
 
 /** @var $botman BotMan */
-$botman->hears('^\^5 {name}', function (BotMan $bot, $name) {
+$botman->hears('^5 {name}', function (BotMan $bot, $name) {
     $bot->reply(':raised_hands: ' . $name);
 });
 
-$botman->hears('^h5 {name}', function (BotMan $bot, $name) {
+$botman->hears('h5 {name}', function (BotMan $bot, $name) {
     $bot->reply(':raised_hands: ' . $name);
 });
 
-$botman->hears('^you rock {name}', function (BotMan $bot, $name) {
+$botman->hears('you rock {name}', function (BotMan $bot, $name) {
     $bot->reply(':muscle: ' . $name);
 });
 
-$botman->hears('^nice {name}', function (BotMan $bot, $name) {
+$botman->hears('nice {name}', function (BotMan $bot, $name) {
     $bot->reply(':+1: ' . $name);
 });
 
-$botman->hears('^gg {name}', function (BotMan $bot, $name) {
+$botman->hears('gg {name}', function (BotMan $bot, $name) {
     $bot->reply(':ok_hand: ' . $name);
 });
