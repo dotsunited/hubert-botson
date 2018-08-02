3235692c9b58c0ee65407246d1faef93b1960cb2
Jonas Regner <jonas.regner@dotsunited.de>
2018-08-02 16:26:30 +0200

    add decoding

--- commands/jokes.php
+++ commands/jokes.php
@@ -7,14 +7,14 @@ $botman->hears('tell me a joke', function (BotMan $bot) {
     $response = file_get_contents('http://api.icndb.com/jokes/random');
     $object = json_decode($response);
 
-    $bot->reply($object->value->joke);
+    $bot->reply(htmlspecialchars_decode($object->value->joke));
 });
 
 $botman->hears('tell me a fred', function (BotMan $bot) {
     $response = file_get_contents('http://api.icndb.com/jokes/random');
     $object = json_decode($response);
 
-    $bot->reply(str_replace('Chuck Norris', 'Fred', $object->value->joke));
+    $bot->reply(htmlspecialchars_decode(str_replace('Chuck Norris', 'Fred', $object->value->joke)));
 });
 
 $botman->hears('.*\b(Essen|essen)\b.*', function (BotMan $bot, $name) {
