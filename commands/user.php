319e376b654b92e71502ac3619aa8812b41738b6
Jonas Regner <jonas.regner@dotsunited.de>
2018-08-03 08:30:01 +0200

    update commands

--- commands/user.php
+++ commands/user.php
@@ -5,13 +5,16 @@ use BotMan\BotMan\BotMan;
 /** @var $botman BotMan */
 $botman->hears('userinfo', function (BotMan $bot) {
     $bot->sendRequest('chat.postEphemeral', [
+        'channel' => $bot->getMessage()->getRecipient(),
         'text' => sprintf(
             'Userinfo: #%s - %s %s - @%s',
             $bot->getUser()->getId(),
             $bot->getUser()->getFirstName(),
             $bot->getUser()->getLastName(),
             $bot->getUser()->getUsername()
-        )
+        ),
+        'user' => $bot->getUser()->getId(),
+        'as_user' => true,
     ]);
 });
 
