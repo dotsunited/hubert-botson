1c4e00d4a273d48ba2f56c3cefb6a407216d656e
Jonas Regner <jonas.regner@dotsunited.de>
2018-08-03 08:49:12 +0200

    update commands

--- commands/user.php
+++ commands/user.php
@@ -8,10 +8,10 @@ $botman->hears('userinfo', function (BotMan $bot) {
         'channel' => $bot->getMessage()->getRecipient(),
         'text' => sprintf(
             '%s %s (%s), id: %s',
-            $bot->getUser()->getId(),
             $bot->getUser()->getFirstName(),
             $bot->getUser()->getLastName(),
-            $bot->getUser()->getUsername()
+            $bot->getUser()->getUsername(),
+            $bot->getUser()->getId()
         ),
         'user' => $bot->getUser()->getId(),
         'as_user' => true,
