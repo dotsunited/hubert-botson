a00c0c8c924afb010f255f6e7b7efb96e31f5433
Jonas Regner <jonas.regner@dotsunited.de>
2018-08-03 11:11:41 +0200

    update commands

--- commands/food.php
+++ commands/food.php
@@ -7,15 +7,15 @@ $botman->hears('.*\b(Essen|essen)\b.*', function (BotMan $bot) {
     $bot->reply(':chompy: :pizza:');
 });
 
-$botman->hears('order at (.*) ?(.*) ?(http.*)', function (BotMan $bot, $restaurant, $user, $link) {
-    $reply = '@here bestellen bei ' . $restaurant . '. ';
+$botman->hears('order (.*) ?(.*)? ?(http.*)?', function (BotMan $bot, $restaurant, $user, $link) {
+    $reply = '@here wir bestellen bei ' . $restaurant . '. Wer möchte mitbestellen?';
 
     if ($user) {
-        $reply .= 'Bestellung an ' . $user;
+        $reply .= 'Bestellung an ' . $user . '. ';
     }
 
     if ($link) {
-        $reply .= ' (' . $link . ')';
+        $reply .= '(' . $link . ')';
     }
 
     $bot->reply($reply);
