234ed7d313da49c328a362ac9e6d17d74bb77022
Jonas Regner <jonas.regner@dotsunited.de>
2018-08-08 09:55:04 +0200

    optimize jira-issue celebration INTISS-20

--- commands/celebrate-jira-issues.php
+++ commands/celebrate-jira-issues.php
@@ -3,7 +3,7 @@
 use BotMan\BotMan\BotMan;
 
 /** @var $botman BotMan */
-$botman->hears('/^\*.*\* transitioned a `.*` from `.*` to `(.*)`/', function (BotMan $bot, $status) {
+$botman->hears('^\*.*\* transitioned a `.*` from `.*` to `(.*)`', function (BotMan $bot, $status) {
     $bot->reply('HALLO WELT');
     $payload = $bot->getMessage()->getPayload();
     if ('BBYVB4RK4' !== $payload['bot_id']) {
