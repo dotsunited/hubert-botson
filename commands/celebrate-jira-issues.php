bdd07c23311cc71fbecd9d13e5414274ce7f5e68
Jonas Regner <jonas.regner@dotsunited.de>
2018-08-08 09:06:17 +0200

    first try celebrating issues INTISS-20

--- commands/celebrate-jira-issues.php
+++ commands/celebrate-jira-issues.php
@@ -4,5 +4,40 @@ use BotMan\BotMan\BotMan;
 
 /** @var $botman BotMan */
 $botman->hears('(.*)', function (BotMan $bot, $pattern) {
-    //$bot->reply(json_encode($bot->getMessage()->getPayload()));
+    if ('Jira Cloud' === $bot->getUser()->getUsername()) {
+        $payload = $bot->getMessage()->getPayload();
+
+        $title = reset($payload['attachments'])['title'];
+        $issueId = 0;
+        if (false !== preg_match('/^([A-Z]+-([0-9]+))/', $title, $matches)) {
+            list(,, $issueId) = $matches;
+        }
+
+        $text = reset($payload['attachments'])['text'];
+        if (false !== preg_match('/^\*.*\* transitioned a `.*` from `.*` to `(.*)`/', $text, $matches)) {
+            $status = $matches[1];
+
+            if (!in_array($status, ['Done', 'Resolved'])) {
+                return;
+            }
+        }
+
+        $files = scandir(__DIR__ . '/../assets/celebrate-jira-issues', SCANDIR_SORT_ASCENDING);
+
+        $celebrate = false;
+        foreach ($files as $file) {
+            if ($file === '.' || $file === '..') {
+                continue;
+            }
+
+            if (false !== strpos($file, $issueId)) {
+                $celebrate = $issueId;
+                break;
+            }
+        }
+
+        if (false !== $celebrate) {
+            $bot->reply('CELEBRATE ISSUE!');
+        }
+    }
 });
