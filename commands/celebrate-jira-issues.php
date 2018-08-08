77ddac9b4e23979bbb39d8b8b91431a46b8c1e3f
Jonas Regner <jonas.regner@dotsunited.de>
2018-08-08 09:56:21 +0200

    optimize jira-issue celebration INTISS-20

--- commands/celebrate-jira-issues.php
+++ commands/celebrate-jira-issues.php
@@ -4,7 +4,6 @@ use BotMan\BotMan\BotMan;
 
 /** @var $botman BotMan */
 $botman->hears('^\*.*\* transitioned a `.*` from `.*` to `(.*)`', function (BotMan $bot, $status) {
-    $bot->reply('HALLO WELT');
     $payload = $bot->getMessage()->getPayload();
     if ('BBYVB4RK4' !== $payload['bot_id']) {
         return;
@@ -21,23 +20,23 @@ $botman->hears('^\*.*\* transitioned a `.*` from `.*` to `(.*)`', function (BotM
         return;
     }
 
-    //$bot->reply($status, $issueId);
+    $bot->reply($status, $issueId);
 
-//    $files = scandir(__DIR__ . '/../assets/celebrate-jira-issues', SCANDIR_SORT_ASCENDING);
-//
-//    $celebrate = false;
-//    foreach ($files as $file) {
-//        if ($file === '.' || $file === '..') {
-//            continue;
-//        }
-//
-//        if (false !== strpos($file, $issueId)) {
-//            $celebrate = $issueId;
-//            break;
-//        }
-//    }
-//
-//    if (false !== $celebrate) {
-//        $bot->reply('CELEBRATE ISSUE!');
-//    }
+    $files = scandir(__DIR__ . '/../assets/celebrate-jira-issues', SCANDIR_SORT_ASCENDING);
+
+    $celebrate = false;
+    foreach ($files as $file) {
+        if ($file === '.' || $file === '..') {
+            continue;
+        }
+
+        if (false !== strpos($file, $issueId)) {
+            $celebrate = $issueId;
+            break;
+        }
+    }
+
+    if (false !== $celebrate) {
+        $bot->reply('CELEBRATE ISSUE!');
+    }
 });
