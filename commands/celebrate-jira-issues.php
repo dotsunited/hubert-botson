b1597cdf8103d8d47d1737d2ff8d674a39233492
Jonas Regner <jonas.regner@dotsunited.de>
2018-08-08 08:36:09 +0200

    try listen to bot events INTISS-20

---
+++ commands/celebrate-jira-issues.php
@@ -0,0 +1,8 @@
+<?php
+
+use BotMan\BotMan\BotMan;
+
+/** @var $botman BotMan */
+$botman->hears('(.*)', function (BotMan $bot, $pattern) {
+    $bot->reply(json_encode($bot->getBotMessages()));
+});
