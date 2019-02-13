30d43c75b392a457cf1845078b00e0248b56f70b
Jonas Regner <jonas.regner@dotsunited.de>
2019-02-13 11:07:53 +0100

    optimize ene mene muh & ene mene mühe

--- commands/jokes.php
+++ commands/jokes.php
@@ -40,40 +40,37 @@ $botman->hears('tell me a joke', function (BotMan $bot) {
 $botman->hears('ene mene muh', function (BotMan $bot) {
     $client = new Client();
 
-    $response = $client->get('https://slack.com/api/users.list', [
+    $response = $client->get('https://slack.com/api/conversations.members', [
         'query' => [
-            'token' => getenv('SLACK_TOKEN')
+            'token' => getenv('SLACK_TOKEN'),
+            'channel' => 'C03DDHLD4'
         ]
     ]);
 
     $json = json_decode($response->getBody()->getContents(), true);
 
-    $members = [];
-    foreach ($json['members'] as $member) {
-        if ($member['is_bot']) { continue; }
-        if ('USLACKBOT' === $member['id']) { continue; }
+    $users = $json['members'];
 
-        $client = new Client();
-
-        $response = $client->get('https://slack.com/api/users.getPresence', [
-            'query' => [
-                'token' => getenv('SLACK_TOKEN'),
-                'user' => $member['id'],
-            ]
-        ]);
-
-        $jsonR = json_decode($response->getBody()->getContents(), true);
-
-        if ('active' === $jsonR['presence']) {
-            $members[$member['id']] = $member['profile'];
-        }
-    }
-
-    $caught = array_rand($members, 1);
+    $caught = array_rand($users, 1);
 
     betterReply($bot, 'und raus bist du <@' . $caught . '>');
 });
 
 $botman->hears('ene mene mühe', function (BotMan $bot) {
-    betterReply($bot, '<@UCBJSUCCV> steht auf Kühe!');
+    $client = new Client();
+
+    $response = $client->get('https://slack.com/api/conversations.members', [
+        'query' => [
+            'token' => getenv('SLACK_TOKEN'),
+            'channel' => 'C03DDHLD4'
+        ]
+    ]);
+
+    $json = json_decode($response->getBody()->getContents(), true);
+
+    $users = $json['members'];
+
+    $caught = array_rand($users, 1);
+
+    betterReply($bot, '<@' .  $caught . '> steht auf Kühe!');
 });
